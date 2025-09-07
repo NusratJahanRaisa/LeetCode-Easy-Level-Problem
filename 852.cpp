42e36d8604d5774c5cdd75a5245a7adfc5c2706c
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2025-09-07 20:54:39 +0600

    vector max function

---
+++ 852.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+
+
+ int peakIndexInMountainArray(vector<int>& arr) {
+        int index = max_element(arr.begin(),arr.end())-arr.begin();
+        return index;
+    }
+
+
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    // before callin function
+    return 0;
+}
